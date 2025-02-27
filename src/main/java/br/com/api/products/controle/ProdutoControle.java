5e402d8e30d4f592e4af4c66558288c9e5cc4085
yamferreira <yamferreira6@gmail.com>
2025-02-27 15:41:23 -0300

    remover

--- src/main/java/br/com/api/products/controle/ProdutoControle.java
+++ src/main/java/br/com/api/products/controle/ProdutoControle.java
@@ -2,12 +2,16 @@ package br.com.api.products.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.products.modelo.ProdutoModelo;
+import br.com.api.products.modelo.RespostaModelo;
 import br.com.api.products.servico.ProdutoServico;
 
 @RestController
@@ -15,6 +19,18 @@ public class ProdutoControle {
 
     @Autowired
     private ProdutoServico ps;
+
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo) {
+        return ps.remover(codigo);
+
+    }
+
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm) {
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
+    
 
     @PostMapping("/cadastrar")
     public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm) {
--- src/main/java/br/com/api/products/servico/ProdutoServico.java
+++ src/main/java/br/com/api/products/servico/ProdutoServico.java
@@ -41,4 +41,12 @@ public class ProdutoServico {
         }
     } 
 
+    public ResponseEntity<RespostaModelo> remover(long codigo) {
+        
+        pr.deleteById(codigo);
+
+        rm.setMensagem("O produto foi removido com sucesso!");
+        return new ResponseEntity<RespostaModelo>(rm, HttpStatus.OK);
+    }
+
 }
